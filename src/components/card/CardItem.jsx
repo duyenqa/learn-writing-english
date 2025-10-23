f5ec58fd61bcb78e75389333c59f4d7720559482
duyenqa <ttemp8399@gmail.com>
2025-10-23 14:48:27 +0700

    refactor change checkbox delete to button delete

--- src/components/card/CardItem.jsx
+++ src/components/card/CardItem.jsx
@@ -1,28 +1,21 @@
 import { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import { toast } from 'react-toastify';
 
 const CardItem = ({ data, removeItem }) => {
     const [isHoverCard, setIsHoverCard] = useState(null);
     const [openModal, setOpenModal] = useState(false);
-    const [checked, setChecked] = useState(false);
-
-    const handleCloseModal = () => {
-        setOpenModal(false);
-        setChecked(false);
-    }
 
     const handleDeleteAndClose = () => {
         removeItem(data.card_id);
         setOpenModal(false);
-        setChecked(false);
         toast("Xóa dữ liệu thành công!");
     };
 
@@ -30,11 +23,7 @@ const CardItem = ({ data, removeItem }) => {
         <>
             <div className="cardItem">
                 <div className="btn-delete" onClick={() => setOpenModal(true)}>
-                    <Checkbox 
-                        inputProps={{ 'aria-label': 'controlled' }} 
-                        checked={checked}
-                        onChange={(event) => setChecked(event.target.value)}
-                    />
+                    <DeleteOutlinedIcon sx={{fontSize: '32px'}}/>
                 </div>
                 <div
                     className={`flip-card-inner ${isHoverCard ? 'hovered' : ''}`}
@@ -51,7 +40,7 @@ const CardItem = ({ data, removeItem }) => {
             </div>
             <Dialog
                 open={openModal}
-                onClose={handleCloseModal}
+                onClose={() => setOpenModal(false)}
                 aria-labelledby="responsive-dialog-title"
             >
                 <DialogTitle id="responsive-dialog-title">
@@ -63,7 +52,7 @@ const CardItem = ({ data, removeItem }) => {
                     </DialogContentText>
                 </DialogContent>
                 <DialogActions>
-                    <Button autoFocus onClick={handleCloseModal}>
+                    <Button autoFocus onClick={() => setOpenModal(false)}>
                         Thoát
                     </Button>
                     <Button onClick={handleDeleteAndClose} autoFocus>
