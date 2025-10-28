88e7a40d095af7f64c0d8d87828c7966fb577cb4
duyenqa <ttemp8399@gmail.com>
2025-10-28 17:45:46 +0700

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,11 +3,9 @@ import Typography from '@mui/material/Typography';
 export default function Footer() {
  return (
    <footer className="footer">
-    <div className="shapeFooter">
       <Typography variant="subtitle2" gutterBottom>
           2025 - {new Date().getFullYear()}
       </Typography>
-    </div>
    </footer>
  );
 }
