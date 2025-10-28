8e133f8d42159c3d567c50a8c60570bd8a115a6a
duyenqa <ttemp8399@gmail.com>
2025-10-28 17:02:57 +0700

    fix bug: responsive

--- src/components/buttons/MultipleButtons.jsx
+++ src/components/buttons/MultipleButtons.jsx
@@ -4,7 +4,20 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 const MultipleButtons = ({nameIcon1, text1, nameIcon2, text2,nameIcon3, text3,randomData,removeAllData,downloadFile}) => {
     
     return (
-        <ButtonGroup variant="contained" aria-label="Basic button group">
+        <ButtonGroup 
+        variant="contained" 
+        aria-label="Basic button group"
+        sx={{
+            flexDirection: {
+                xs: 'column',
+                sm: 'row',   
+            },
+            width: {
+                xs: '100%', 
+                sm: 'auto',
+            },
+            flexWrap: 'wrap'
+        }}>
             <Button startIcon={nameIcon1} onClick={() => randomData()}>{text1}</Button>
             <Button startIcon={nameIcon2} onClick={() => downloadFile()}>{text2}</Button>
             <Button startIcon={nameIcon3} onClick={() => removeAllData()}>{text3}</Button>
