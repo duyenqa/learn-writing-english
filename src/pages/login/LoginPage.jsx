f9b8376880b9765d866e3ba6095493b105bbd766
duyenqa <ttemp8399@gmail.com>
2025-10-29 14:26:45 +0700

    update ui login page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,18 +1,67 @@
 import { useNavigate } from 'react-router-dom';
+import {useState} from 'react';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './styles.css';
 
 function LoginPage() {
   const navigate = useNavigate();
+  const[email, setEmail] = useState(" ");
+  const[password, setPassword] = useState(" ");
+  const[errorEmail, setErrorEmail] = useState(" ");
+  const[errorPassword, setErrorPassword] = useState(" ");
 
-  function handleSubmit(){
-    navigate('/home');
+  function handleChangeEmail(event) {
+    setEmail(event.target.value);
+    setErrorEmail(" ");
   }
- return (
-   <div className="loginPage">
-    <h1>Page Login</h1>
-    <Button variant="contained" onClick={handleSubmit}>Log In</Button>
-   </div>
- )
+
+  function handleChangePassword(event) {
+    setPassword(event.target.value);
+    setErrorPassword(" ");
+  }
+
+  function handleSubmit() {
+    if(!email.trim() && !password.trim()){
+      setErrorEmail("Không được để trống!");
+      setErrorPassword("Không được để trống!");
+    }else if(!email.trim() && !!password.trim()){
+      setErrorEmail("Không được để trống!");
+    }else if(!!email.trim() && !password.trim()){
+      setErrorPassword("Không được để trống!");
+    }else{
+      navigate('/home');
+      console.log(email, password);
+    }
+  }
+
+  return (
+    <div className="loginPage">
+      <div className="formLogin">
+        <TextField
+          id="standard-basic"
+          label="Email"
+          variant="standard"
+          value={email}
+          onChange={handleChangeEmail}
+          required
+        />
+        {errorEmail && (<p className="errorMessage">{errorEmail}</p>)}
+        <TextField
+          id="standard-password-input"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          variant="standard"
+          value={password}
+          onChange={handleChangePassword}
+          required
+        />
+        {errorPassword && (<p className="errorMessage">{errorPassword}</p>)}
+        <Button variant="contained" onClick={handleSubmit}>Đăng Nhập</Button>
+      </div>
+    </div>
+  )
 };
 
 export default LoginPage;
