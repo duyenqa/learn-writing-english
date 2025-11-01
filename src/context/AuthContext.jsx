ede05cd9f49ef85972aaf5bc92b9abd254e83d98
duyenqa <ttemp8399@gmail.com>
2025-11-01 15:24:39 +0700

    fix bug: miss data when user enters F5 to refresh page

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,6 +5,7 @@ const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
     const [session, setSession] = useState(undefined);
+    const [loading, setLoading] = useState(true);
 
     const signUpUser = async (email, password) => {
         const { data, error } = await supabase
@@ -18,22 +19,22 @@ export const AuthContextProvider = ({ children }) => {
     }
 
     const signOut = async () => {
-        const {error} = await supabase.auth.signOut();
-        if(error) console.error(error.message);
+        const { error } = await supabase.auth.signOut();
+        if (error) console.error(error.message);
     }
 
     const signInUser = async (email, password) => {
-        try{
-            const {data, error} = await supabase.auth.signInWithPassword({email, password});
-            if(error){
+        try {
+            const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+            if (error) {
                 console.error("sign in error occurred: ", error);
                 return {
                     success: false,
                     error: "Đăng nhập thất bại!!!"
                 }
             }
-            return {success: true, data};
-        }catch(error){
+            return { success: true, data };
+        } catch (error) {
             return {
                 success: false,
                 error: "Lỗi không xác định!!!"
@@ -46,22 +47,22 @@ export const AuthContextProvider = ({ children }) => {
             const { data } = await supabase.auth.getSession();
             if (data && data.session) {
                 setSession(data.session);
+                setLoading(false);
             }
         }
-
-        function monitorUserEventActions() {
-            const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-                setSession(session);
-            });
-            return authListener;
-        }
-
         getInformationSignIn();
-        monitorUserEventActions();
+
+        const { data: authListener } = supabase.auth.onAuthStateChange((_event, newSession) => {
+            setSession(newSession);
+            setLoading(false);
+        })
+        return () => {
+            authListener.subscription.unsubscribe();
+        }
     }, []);
 
     return (
-        <AuthContext.Provider value={{ session, signUpUser, signOut, signInUser }}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{ session, loading, signUpUser, signOut, signInUser }}>{children}</AuthContext.Provider>
     )
 }
 export const useAuth = () => {
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,15 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
+import CircularProgress from '@mui/material/CircularProgress';
 
-const PrivateRoute = ({children}) => {
-    const {session} = useAuth();
+const PrivateRoute = ({ children }) => {
+    const { loading, session } = useAuth();
+
+    if (loading) {
+        return <CircularProgress size="3rem" />;
+    }
     return <>
-        {session ? <>{children}</> : <Navigate to="/sign-up"/>}
+        {session ? <>{children}</> : <Navigate to="/sign-up" />}
     </>
 }
 export default PrivateRoute;
