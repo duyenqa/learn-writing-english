bc7d1336fe1a5f7324e8782acc3507c3251efc1a
duyenqa <ttemp8399@gmail.com>
2026-03-13 13:54:04 +0700

    remove click outside

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { useNotification } from '../../context/MessageContext';
@@ -19,7 +19,6 @@ function LoginPage() {
   const [errorEmail, setErrorEmail] = useState(" ");
   const [errorPassword, setErrorPassword] = useState(" ");
   const [showPassword, setShowPassword] = useState(false);
-  const [isActive, setIsActive] = useState(false);
   const inputEmailRef = useRef(null);
   const inputPasswordRef = useRef(null);
   const { signInUser } = useAuth();
@@ -90,32 +89,6 @@ function LoginPage() {
       }
     }
   }
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (inputPasswordRef.current !== null) {
-        if (!inputPasswordRef.current.contains(event.target)) {
-          if(inputPasswordRef.current.value?.length <= 0){
-            setErrorPassword("Không được để trống!");
-          }else if(inputPasswordRef.current.value?.length > 0 && inputPasswordRef.current.value?.length < 8){
-            setErrorPassword("Mật khẩu phải tối thiểu 8 ký tự!");
-          }else{
-            setErrorPassword(" ");
-            setIsActive(false);
-          }
-        }
-      }
-
-    }
-
-    if (isActive) {
-      window.addEventListener("click", handleClickOutside);
-    }
-
-    return () => {
-      window.addEventListener("click", handleClickOutside);
-    }
-  }, [])
 
   return (
     <section className="loginPage">
